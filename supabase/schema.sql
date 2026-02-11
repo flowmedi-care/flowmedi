864cd92836e33db45a110ae65128e67127658157
constiva <flowmedi.care@gmail.com>
2026-02-11 19:42:45 -0300

    t1

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -118,7 +118,7 @@ CREATE TABLE IF NOT EXISTS public.message_log (
   metadata jsonb DEFAULT '{}'
 );
 
--- ========== RLS (Row Level Security) ==========
+-- ========== RLS (Row Levels Security) ==========
 ALTER TABLE public.plans ENABLE ROW LEVEL SECURITY;
 ALTER TABLE public.clinics ENABLE ROW LEVEL SECURITY;
 ALTER TABLE public.profiles ENABLE ROW LEVEL SECURITY;
