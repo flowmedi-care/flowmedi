8d3f4a5d15f08c4b429b3b8e8b120e3e2f598438
constiva <flowmedi.care@gmail.com>
2026-02-15 08:00:29 -0300

    4

--- app/dashboard/consulta/consulta-client.tsx
+++ app/dashboard/consulta/consulta-client.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useMemo, useEffect } from "react";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
